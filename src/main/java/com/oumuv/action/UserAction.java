a30ae48db39982f9f378796ccef491e3eb29e950
Administrator <Administrator@PC-20160726001>
2017-08-22 16:26:05 +0800

    first commit

---
+++ src/main/java/com/oumuv/action/UserAction.java
@@ -0,0 +1,17 @@
+package com.oumuv.action;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserAction {
+
+	@RequestMapping("hi")
+	private String sayhi() {
+		// TODO Auto-generated method stub
+		System.out.println("hello word!");
+		return "login";
+	}
+	
+}
