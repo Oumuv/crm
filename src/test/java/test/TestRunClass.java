ac233f06253bf93a46dcf9bdb3f7d473ce8348aa
Oumuv <1134420025@qq.com>
2017-12-07 11:44:21 +0800

    备份提交2017.12.7

--- src/test/java/test/TestRunClass.java
+++ src/test/java/test/TestRunClass.java
@@ -9,26 +9,11 @@ import org.junit.Test;
 
 public class TestRunClass {
 	
-	@Test
+	
+	public static void main(String[] args) {
+	}
 	public void run(){
-		List<Long> l1 = new ArrayList<Long>();
-		List<Long> l2 = new ArrayList<Long>();
-//		l1.add(1L);
-		l1.add(2L);
-		l1.add(3L);
-//		l1.add(4L);
-//		l1.add(5L);
-		
-//		l2.add(4L);
-//		l2.add(5L);
-//		l2.add(6L);
-		l2.add(2L);
-		l2.add(8L);
-		
-		List<Long> del = setDifferenceSet(l1,l2);
-		List<Long> add = setDifferenceSet(l2,l1);
-		int i=0;
-		
+			
 	}
 	
 	/**
@@ -41,4 +26,8 @@ public class TestRunClass {
 		list.removeAll(list2);
 		return list;
 	}
+	
+	
+	
+	
 }
